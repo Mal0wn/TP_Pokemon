eff094448988dcbaef3e74687027b5c161ddcad0
Marine M <malomclt@gmail.com>
2022-04-06 10:04:47 +0200

    Create class dresseur

---
+++ Classes/Dresseur.ts
@@ -0,0 +1,27 @@
+export class Dresseur {
+
+	_nom : string;
+
+/**
+ * 
+ * @param number : Methode qui va permettre d'ajouter des Pokeballs dans l'inventaire du dresseur 
+ */
+	ajouterPokeballs( number) : void {
+
+	}
+
+/**
+ * 
+ * @param pokemon : Methode qui va prendre en parametre un pokemon et le stocker dans une pokeball 
+ */
+	capture(pokemon) : void {
+	
+	}
+/**
+ * Methode qui récupère la liste des Pokémons dans l'inventaire du dresseur ?
+ */
+	getPokemon( ) : void {
+	
+	}
+
+}
