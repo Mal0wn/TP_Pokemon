c4cd62efc914056955de256db2a4ee15c371d0ac
Marine M <malomclt@gmail.com>
2022-04-06 10:40:08 +0200

    update Pokeball methods

--- Classes/Pokeball.ts
+++ Classes/Pokeball.ts
@@ -8,21 +8,20 @@ export class Pokeball {
 
 constructor( contient : Pokemon , proprietaire : Dresseur) {
 
-	this._contient = contient;
-	this._proprietaire = proprietaire;
+	contient = this._contient;
+	proprietaire = this._proprietaire;
 }
 
-	attaquer(): void {
-		throw new Error("Method not implemented.");
-	}
-	subirAttaque(): void {
-		throw new Error("Method not implemented.");
-	}
-	soigner(): void {
-		throw new Error("Method not implemented.");
-	}
-	renommer(): void {
-		throw new Error("Method not implemented.");
+/**
+ * Donne le contenu de la Pookeball donc soit vide ou contient un Pokemon et lequel
+ */
+	getContenu() : void {
+this.contient.nom 
+
+	};
+
+	affecterPokemon() : void {
+	
 	}
  
 
