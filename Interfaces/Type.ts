d40a3e7b979089ac7aba9b8c6befea073442d996
Marine M <malomclt@gmail.com>
2022-04-06 09:47:49 +0200

    create interface Type

---
+++ Interfaces/Type.ts
@@ -0,0 +1,20 @@
+export interface Type {
+	_degats : number;
+
+
+/**
+ * Méthode qui permet d'attaquer un adversaire de type Feu
+ */
+	attaquerFeu() : void ;
+
+/**
+ * Méthode qui permet d'attaquer un adversaire de type Eau
+ */
+	attaquerEau() : void ; 
+
+/**
+ * Méthode qui permet d'attaquer un adversaire de type Plante
+ */
+	attaquerPlante() : void ; 
+
+}
